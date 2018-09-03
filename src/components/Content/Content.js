8ea5c6bb862cdfe33bce39bff1010c140033a471
Arthur Melo <contact@arthurmelo.com>
2018-09-03 11:56:41 -0300

    Refactored scaling for responsive design

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -31,38 +31,45 @@ const contentDefaultProps = {
 const Content = props => (
   <div className={props.className}>
     <div className="container">
-      <div className="card text-center">
-        <div className="card-body">
-          <h5 className="card-title">Welcome!</h5>
-          <p className="card-text">To get started, add some items to your list:</p>
-          <div className="d-inline-block">
-            <Form handleAddItem={props.handleAddItem} />
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="card text-center">
+            <div className="card-body">
+              <h5 className="card-title">Welcome!</h5>
+              <p className="card-text">To get started, add some items to your list:</p>
+              <div className="d-inline-block">
+                <Form handleAddItem={props.handleAddItem} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="content__todos">
-        <ul className="list-group content__todos__ul">
-          {props.items.map(
-            (item, index) =>
-              props.editingItem.id === item.id ? (
-                <FormEdit
-                  key={index}
-                  handleEditItem={props.handleEditItem}
-                  handleCancelEditItem={props.handleCancelEditItem}
-                  item={props.editingItem}
-                />
-              ) : (
-                <FormItem
-                  key={index}
-                  item={item}
-                  handleDeleteItem={props.handleDeleteItem}
-                  handleSelectEditItem={props.handleSelectEditItem}
-                  handleItemCompletion={props.handleItemCompletion}
-                />
-              ),
-          )}
-        </ul>
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="content__todos">
+            <ul className="list-group content__todos__ul">
+              {props.items.map(
+                (item, index) =>
+                  props.editingItem.id === item.id ? (
+                    <FormEdit
+                      key={index}
+                      handleEditItem={props.handleEditItem}
+                      handleCancelEditItem={props.handleCancelEditItem}
+                      item={props.editingItem}
+                    />
+                  ) : (
+                    <FormItem
+                      key={index}
+                      item={item}
+                      handleDeleteItem={props.handleDeleteItem}
+                      handleSelectEditItem={props.handleSelectEditItem}
+                      handleItemCompletion={props.handleItemCompletion}
+                    />
+                  ),
+              )}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
