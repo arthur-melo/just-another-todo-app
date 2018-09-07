014558ccb44bc4ab5fe983dcd7783b6ed78e0053
Arthur Melo <arthrmlo@gmail.com>
2018-09-07 13:54:52 -0300

    Refactoring

--- src/components/Layout/App.js
+++ src/components/Layout/App.js
@@ -9,9 +9,7 @@ import Footer from '../Footer/Footer';
 const App = props => (
   <div className="app__container">
     <Header className="app__header" />
-
     <Content {...props} className="app__content" />
-
     <Footer className="app__footer" />
   </div>
 );
