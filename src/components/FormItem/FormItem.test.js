0f803793ce773d41503cc63178340e3e36d1b62d
Arthur Melo <arthrmlo@gmail.com>
2018-09-18 21:50:00 -0300

    Added FormItem tests

---
+++ src/components/FormItem/FormItem.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import FormItem from './FormItem';
+import PropertyBar from '../PropertyBar/PropertyBar';
+
+describe('FormItem', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      item: {
+        value: 'someValue',
+        id: '0',
+        completed: false,
+      },
+      handleDeleteItem: jest.fn(),
+      handleSelectEditItem: jest.fn(),
+      handleItemCompletion: jest.fn(),
+    };
+  });
+
+  it('verify if handleItemCompletion prop is being called', () => {
+    const component = shallow(<FormItem {...props} />);
+
+    component
+      .find('.form_item__component')
+      .children()
+      .first()
+      .simulate('click');
+
+    expect(props.handleItemCompletion).toHaveBeenCalledWith(props.item);
+    expect(props.handleDeleteItem).not.toHaveBeenCalled();
+    expect(props.handleSelectEditItem).not.toHaveBeenCalled();
+  });
+
+  it('verify if setDisplayMenu updates displayMenu', () => {
+    const component = shallow(<FormItem {...props} />);
+
+    expect(component.state('displayMenu')).toBe(false);
+
+    component.instance().setDisplayMenu(true);
+    expect(component.state('displayMenu')).toBe(true);
+  });
+
+  it('verify if displayMenu instantiates a PropertyBar component', () => {
+    const component = shallow(<FormItem {...props} />).update();
+
+    expect(component.state('displayMenu')).toBe(false);
+    expect(component.find(PropertyBar).length).toBe(0);
+
+    component.instance().setDisplayMenu(true);
+    expect(component.find(PropertyBar).length).toBe(1);
+  });
+});
