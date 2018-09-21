5bc168d195d0b124d33bb3247b2f535619cdc22e
Arthur Melo <arthrmlo@gmail.com>
2018-09-20 21:05:53 -0300

    Added Form tests

---
+++ src/components/Form/Form.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import Form from './Form';
+
+describe('FormEdit', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      item: '',
+      handleAddItem: jest.fn(),
+    };
+  });
+
+  it('verify if form submission is being handled', () => {
+    const component = shallow(<Form {...props} />);
+
+    component.find('form').simulate('submit', { preventDefault: jest.fn() });
+
+    expect(props.handleAddItem).toHaveBeenCalledWith(props.item);
+  });
+
+  it('verify if form submission is handled with a new input field value', () => {
+    const value = 'someNewValue';
+
+    const component = shallow(<Form {...props} />);
+
+    component.find('#new-todo-item').simulate('change', { target: { value } });
+
+    expect(component.state('itemValue')).toBe(value);
+
+    component.find('form').simulate('submit', { preventDefault: jest.fn() });
+
+    expect(props.handleAddItem).toHaveBeenCalledWith(value);
+  });
+});
