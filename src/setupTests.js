8a255ed52f26281c1e211430298592923b53ff24
Arthur Melo <8452523+arthur-melo@users.noreply.github.com>
2018-10-02 19:16:49 -0300

    Updated react-script to v2 (#48)

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,8 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import 'jest-localstorage-mock';
 
+jest.spyOn(Storage.prototype, 'setItem');
+jest.spyOn(Storage.prototype, 'getItem');
+jest.spyOn(Storage.prototype, 'removeItem');
+
 Enzyme.configure({ adapter: new Adapter() });
